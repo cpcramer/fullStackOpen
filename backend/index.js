d08951d450f5d098583f959293559362e9f67c51
CHAD CRAMER <chadpcramer@gmail.com>
2020-06-10 10:48:02 -0700

    gitingore

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,6 @@ app.use(express.json());
 app.use(cors())
 app.use(express.static('build'))
 
-
-
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
